f30339f9d415adf8e4cb111daa13f6e47b10acca
Aleksej Smirnov <lyolek@gmail.com>
2017-11-04 19:02:14 +0200

    первый вариант

---
+++ heaterSwitch/heaterSwitch.ino
@@ -0,0 +1,104 @@
+#include "DHT.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "Mega";
+const char* password = "inter1017net";
+const char* usr = "dev2";
+const char* pwd = "dev2dev2";
+
+unsigned long lustRun;
+
+#define DHTPIN 14
+
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setDebugOutput(true);
+  Serial.println("Booting");
+
+  WiFi.begin(ssid, password);
+  String hostName = "esp8266:";
+  hostName += usr;
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  dht.begin();
+  
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  setDefaultPortValues();
+}
+
+void setDefaultPortValues() {
+  digitalWrite(12, 1);
+  digitalWrite(13, 0);
+}
+
+void loop() {
+  
+  if(millis() - lustRun > 5000) {
+    lustRun = millis();
+    float t = dht.readTemperature();
+    Serial.printf("t=%d\n", t);
+  
+    if (isnan(t)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+  
+    // wait for WiFi connection
+    if((WiFi.status() == WL_CONNECTED)) {
+  
+      HTTPClient http;
+      Serial.println("[HTTP] start...");
+      String url = "http://iot.lyolek.dp.ua/services/device.php?GPIO14=";
+      url += t;
+      http.begin(url);
+      http.setAuthorization(usr, pwd);
+  
+      int httpCode = http.GET();
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+      if(httpCode == 200) {
+        String resp = http.getString();
+        char JSONMessage[resp.length() + 1];
+        resp.toCharArray(JSONMessage, resp.length() + 1);
+        Serial.println(JSONMessage);
+        StaticJsonBuffer<300> JSONBuffer;   //Memory pool
+        JsonObject& parsed = JSONBuffer.parseObject(JSONMessage); //Parse message
+        if (!parsed.success()) {   //Check for errors in parsing
+  
+          Serial.println("Parsing failed");
+          delay(5000);
+          return;
+        }
+        float GPIO12 = parsed["GPIO12"];
+        Serial.printf("GPIO12=%d\n", GPIO12);
+  
+        if(t < (GPIO12 - 0.5)) {
+          digitalWrite(12, 1);
+          Serial.println("Set ON");
+        } else if(t > (GPIO12 + 0.5)) {
+          digitalWrite(12, 0);
+          Serial.println("Set OFF");
+        }
+        digitalWrite(13, 1);
+      } else {
+          Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+          setDefaultPortValues();
+      }
+      http.end();
+    } else {
+      setDefaultPortValues();
+    }
+    
+  }
+
+}
